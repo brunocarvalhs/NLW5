55adbb9dfe938d17f846d881bae8f82c98e35088
Brunna Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2021-04-20 09:33:36 -0300

    Inicio da aula 2

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,6 +14,26 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBarWidget(
         name: "Bruno Carvalho",
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            LevelButtonWidget(
+              label: "Fácil",
+            ),
+            LevelButtonWidget(
+              label: "Médio",
+            ),
+            LevelButtonWidget(
+              label: "Difícil",
+            ),
+            LevelButtonWidget(
+              label: "Perito",
+            ),
+          ],
+        ),
       ),
     );
   }
