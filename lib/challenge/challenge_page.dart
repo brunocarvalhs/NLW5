f3b54d1a51be24e250515718af044a6cd908fc1f
Brunna Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2021-04-22 09:36:09 -0300

    Metade da Aula 4

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,8 +1,13 @@
+import 'package:DevQuiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:DevQuiz/challenge/widgets/question_indication/question_indicator_widget.dart';
 import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
 class ChallengePage extends StatefulWidget {
+  final List<QuestionModel> questions;
+
+  const ChallengePage({Key? key, required this.questions}) : super(key: key);
   @override
   _ChallengePageState createState() => _ChallengePageState();
 }
@@ -13,7 +18,42 @@ class _ChallengePageState extends State<ChallengePage> {
     return Scaffold(
       appBar: QuestionIndicationWidget(),
       body: QuizWidget(
-        title: "O que o Flutter faz em sua totalidade?",
+        question: widget.questions[0],
+      ),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              (0 == 0)
+                  ? Expanded(
+                      child: NextButtonWidget.white(
+                        label: "Voltar",
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    )
+                  : Expanded(
+                      child: NextButtonWidget.white(
+                        label: "Pular",
+                        onTap: () {},
+                      ),
+                    ),
+              SizedBox(
+                width: 7,
+              ),
+              Expanded(
+                child: NextButtonWidget.green(
+                  label: "Confirma",
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,10 +1,11 @@
 import 'package:DevQuiz/challenge/widgets/awnser/awnser_widget.dart';
 import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
 class QuizWidget extends StatelessWidget {
-  final String title;
-  const QuizWidget({Key? key, required this.title}) : super(key: key);
+  final QuestionModel question;
+  const QuizWidget({Key? key, required this.question}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,32 +13,24 @@ class QuizWidget extends StatelessWidget {
       child: Column(
         children: [
           Text(
-            title,
+            question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(
             height: 24,
           ),
-          AwnserWidget(
-            isRight: false,
-            isSelected: true,
-            title: "Kit de desenvolvimento de interface de usuário",
-          ),
-          AwnserWidget(
-            isRight: true,
-            isSelected: true,
-            title:
-                "Possibilita a criação de aplicativos compilados nativamente",
-          ),
-          AwnserWidget(
-            isRight: false,
-            isSelected: false,
-            title: "Acho que é uma marca de café do Himalaia",
-          ),
-          AwnserWidget(
-            isRight: false,
-            isSelected: false,
-            title: "Possibilita a criação de desktops que são muito incríveis",
+          Expanded(
+            child: ListView(
+              children: [
+                ...question.awnsers
+                    .map((e) => AwnserWidget(
+                          isRight: e.isRight,
+                          isSelected: false,
+                          title: e.title,
+                        ))
+                    .toList(),
+              ],
+            ),
           ),
         ],
       ),
